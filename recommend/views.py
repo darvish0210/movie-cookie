0c65c8c75ac07f99d5a9caf47cae2664c4d952b8
Haesun-Pyeon <sme1357@naver.com>
2023-12-21 01:36:30 +0900

    ⚡️ UPDATE: 추천 모델 정규화에 맞춰 변경

--- recommend/views.py
+++ recommend/views.py
@@ -11,13 +11,13 @@ from .serializers import RecommendSerializer
 from .schema_examples import (
     GENERATE_REQUEST,
     GENERATE_RESPONSE,
-    RECOMMEND_LIST,
     RECOMMEND_REQUEST,
     RECOMMEND_RESPONSE,
+    RECOMMEND_LIST,
 )
-from movieinfo.models import MovieInfo
+from movieinfo.models import MovieInfo, Poster
 from movieinfo.serializers import MovieInfoSerializers
-from movieinfo.utils import saveMovieInfo
+from movieinfo.utils import save_movie_info
 
 import json
 import requests
@@ -87,22 +87,32 @@ class RecommendViewSet(ModelViewSet):
         serializer.save(movie=movie)
 
     # 추천 영화 생성하는 함수
-    @action(detail=False, methods=["POST"])
+    @action(detail=False, methods=["POST", "GET"])
     def generate(self, request):
+        if request.method == "GET":
+            return Response({"message": "Generate GET"}, status=200)
         serializer = RecommendSerializer(data=request.data)
         if serializer.is_valid():
             # 입력값으로 필터링 된 영화 리스트 받아오기
-            input_nation = serializer.validated_data.get("input_nation")
-            input_period = serializer.validated_data.get("input_period")
-            input_genre = serializer.validated_data.get("input_genre")
+            genre = serializer.validated_data.get("genre")
+            nation_korean = serializer.validated_data.get("nation_korean")
+            nation_foreign = serializer.validated_data.get("nation_foreign")
+            period_2000 = serializer.validated_data.get("period_2000")
+            period_2010 = serializer.validated_data.get("period_2010")
+            period_2020 = serializer.validated_data.get("period_2020")
             selected_movies = self.get_movie_list(
-                input_nation, input_period, input_genre
+                genre,
+                nation_korean,
+                nation_foreign,
+                period_2000,
+                period_2010,
+                period_2020,
             )
 
             # 유저가 로그인한 경우 선호 장르와 좋아요한 영화 정보로 가중치 높이기
             if request.user.is_authenticated:
                 user = request.user
-                selected_movies = self.update_weight(user, selected_movies)
+                # selected_movies = self.update_weight(user, selected_movies)
 
             # 가중치, 관객수 기준으로 내림차순 정렬
             selected_movies = selected_movies.sort_values(
@@ -113,14 +123,20 @@ class RecommendViewSet(ModelViewSet):
             top_10 = selected_movies.head(10)
             pick = top_10.sample(n=1)
             movie = self.get_movieinfo(pick)
+            poster_id = movie.data["posters"][0]
+            poster_url = Poster.objects.get(id=poster_id).url
 
             # 모델 형식에 맞게 리턴
             recommended_data = {
-                "input_nation": input_nation,
-                "input_period": input_period,
-                "input_genre": input_genre,
+                "genre": [g.genre for g in genre],
+                "nation_korean": nation_korean,
+                "nation_foreign": nation_foreign,
+                "period_2000": period_2000,
+                "period_2010": period_2010,
+                "period_2020": period_2020,
                 "movie_id": movie.data["id"],
-                "movie": movie.data,
+                "movie_title": movie.data["title"],
+                "poster_url": poster_url,
             }
             return Response(recommended_data)
         else:
@@ -128,88 +144,95 @@ class RecommendViewSet(ModelViewSet):
 
     # 입력값으로 추천영화 csv파일을 필터링해서 리턴
     @staticmethod
-    def get_movie_list(nation, period, genres):
+    def get_movie_list(
+        genres,
+        nation_korean,
+        nation_foreign,
+        period_2000,
+        period_2010,
+        period_2020,
+    ):
         # nation - 국내, 해외, 국내|해외 여부에 따라 CSV 파일 선택
-        if "국내" in nation and "해외" in nation:
-            korean_data = pd.read_csv("static/korean.csv")
-            foreign_data = pd.read_csv("static/foreign.csv")
-            movies_data = pd.concat([korean_data, foreign_data])
-        elif "국내" in nation:
-            movies_data = pd.read_csv("static/korean.csv")
-        elif "해외" in nation:
-            movies_data = pd.read_csv("static/foreign.csv")
-        else:
+        movies_data = pd.DataFrame()
+        if nation_korean:
+            movies_data = pd.concat([movies_data, pd.read_csv("static/korean.csv")])
+        if nation_foreign:
+            movies_data = pd.concat([movies_data, pd.read_csv("static/foreign.csv")])
+        if movies_data.empty:
             return Response({"message": "Invalid input_nation value"}, status=400)
 
         # period - 개봉연도 조건에 따른 필터링
-        movies = pd.DataFrame()
-        if "2000년대" in period:
-            movies = pd.concat([movies, movies_data[movies_data["개봉연도"] < 2010]])
-        if "2010년대" in period:
-            movies = pd.concat(
+        movies_period = pd.DataFrame()
+        if period_2000:
+            movies_period = pd.concat(
+                [movies_period, movies_data[movies_data["개봉연도"] < 2010]]
+            )
+        if period_2010:
+            movies_period = pd.concat(
                 [
-                    movies,
+                    movies_period,
                     movies_data[
                         (movies_data["개봉연도"] >= 2010) & (movies_data["개봉연도"] < 2020)
                     ],
                 ]
             )
-        if "2020년대" in period:
-            movies = pd.concat([movies, movies_data[movies_data["개봉연도"] >= 2020]])
-        movies_data = movies
+        if period_2020:
+            movies_period = pd.concat(
+                [movies_period, movies_data[movies_data["개봉연도"] >= 2020]]
+            )
+        movies_data = movies_period
         if movies_data.empty:
             return Response({"message": "Invalid input_period value"}, status=400)
 
         # genres - 장르를 |로 나눠서 영화 필터링
-        movie_list = pd.DataFrame()
-        for genre in genres.split("|"):
-            movie_list = pd.concat(
+        movies_list = pd.DataFrame()
+        for genre in genres:
+            genre = genre.genre
+            movies_list = pd.concat(
                 [
-                    movie_list,
+                    movies_list,
                     movies_data[movies_data["kmdb장르"].str.contains(genre)],
                 ]
             )
-        if movie_list.empty:
+        if movies_list.empty:
             return Response({"message": "Invalid input_genre value"}, status=400)
 
         # 중복 제거, 가중치 컬럼 생성
-        movie_list = movie_list.drop_duplicates(subset="번호")
-        movie_list["가중치"] = 0
-        return movie_list
+        movies_list = movies_list.drop_duplicates(subset="번호")
+        movies_list["가중치"] = 0
+        return movies_list
 
     # 유저의 선호장르태그, 좋아요한 영화에 따른 가중치 부여
     @staticmethod
     def update_weight(user, movie_list):
         # accounts 모델 내용에 따라 달라질 예정
-        # 선호장르태그 - 일단 profile의 tag로 연결
-        user_genres = user.profile.tag.split("|") if user.profile.tag else []
+        # 선호장르태그 - 일단 genre로 연결
+        user_genres = user.genre.all() if user.genre.all() else []
         # 좋아요한 영화 - LikeMovie의 user의 related name (수정예정)
-        user_liked_movies = user.like.all()
+        user_liked_movies = user.likes.all()
 
         # 선호장르 가중치 부여
         for genre in user_genres:
-            movie_list.loc[movie_list["kmdb장르"].str.contains(genre), "가중치"] += 3
+            movie_list.loc[movie_list["kmdb장르"].str.contains(genre.genre), "가중치"] += 3
 
         # 영화 좋아요에 따른 가중치 부여
         for liked_movie in user_liked_movies:
             # user로 필터링된 LikeMovie모델 - 영화 - 장르
             liked_genre = liked_movie.movie.genres.split("|")
             for genre in liked_genre:
-                movie_list.loc[movie_list["kmdb장르"].str.contains(genre), "가중치"] += 1
+                movie_list.loc[
+                    movie_list["kmdb장르"].str.contains(genre.genre), "가중치"
+                ] += 1
 
         return movie_list
 
     # 영화정보 객체 받아오기
     @staticmethod
     def get_movieinfo(movie_row):
-        movie_title = movie_row.iloc[0]["kmdb제목"]
-        movie_genres = movie_row.iloc[0]["kmdb장르"]
         movie_id = movie_row.iloc[0]["movie_id"]
         movie_seq = str(movie_row.iloc[0]["movie_seq"]).zfill(5)
-
-        movie_info = MovieInfo.objects.filter(
-            title=movie_title, genres=movie_genres
-        ).first()
+        docid = movie_id + movie_seq
+        movie_info = MovieInfo.objects.filter(docid=docid).first()
         if not movie_info:  # 기존 DB에 없을 경우 새로 요청 후 저장하고 불러옴
             # 영화 정보 KMDB API 요청
             KMDB_API_KEY = str(settings.KMDB_API_KEY)
@@ -218,10 +241,8 @@ class RecommendViewSet(ModelViewSet):
             if res.status_code == 200:
                 response_body = res.content
                 data = json.loads(response_body.decode("utf-8"))
-                saveMovieInfo(data)
-                movie_info = MovieInfo.objects.filter(
-                    title=movie_title, genres=movie_genres
-                ).first()
+                save_movie_info(data)
+                movie_info = MovieInfo.objects.filter(docid=docid).first()
             else:
                 return Response({"message": "KMDB Error"}, status=400)
         movie = MovieInfoSerializers(movie_info)
