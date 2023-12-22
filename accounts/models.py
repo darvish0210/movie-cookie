c172439b5ed5e3ecd9c6b7c98ca0829ab1884934
bg.lee <qudrhkslee@gmail.com>
2023-12-22 13:53:46 +0900

    ✨ADD: 프로필 수정기능 추가

--- accounts/models.py
+++ accounts/models.py
@@ -10,3 +10,29 @@ class User(AbstractUser):
         upload_to="profile_pictures/", null=True, blank=True
     )
     bio = models.TextField(blank=True)
+
+
+class LikeMovie(models.Model):
+    id = models.AutoField(primary_key=True)
+    movie = models.ForeignKey(
+        "MovieInfo", on_delete=models.CASCADE, related_name="likes"
+    )
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="likes")
+
+
+class WatchedMovie(models.Model):
+    id = models.AutoField(primary_key=True)
+    movie = models.ForeignKey(
+        "MovieInfo", on_delete=models.CASCADE, related_name="watcheds"
+    )
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="watcheds")
+
+
+class WatchlistMovie(models.Model):
+    id = models.AutoField(primary_key=True)
+    movie = models.ForeignKey(
+        "MovieInfo", on_delete=models.CASCADE, related_name="watchlists"
+    )
+    user = models.ForeignKey(
+        "User", on_delete=models.CASCADE, related_name="watchlists"
+    )
